12d54b03e5b1d63ca0ea6a328f7a0ba5399846a4
Andros Fenollosa <andros@fenollosa.email>
2020-05-29 20:32:45 +0200

    Add list

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@
 import hljs from 'highlight.js/lib/core';
 import shell from 'highlight.js/lib/languages/shell';
 import clojure from 'highlight.js/lib/languages/clojure';
+import yaml from 'highlight.js/lib/languages/yaml';
+import django from 'highlight.js/lib/languages/django';
 
 // Load languages highlight
 hljs.initHighlightingOnLoad();
 hljs.registerLanguage('shell', shell);
 hljs.registerLanguage('clojure', clojure);
-hljs.registerLanguage('yaml', clojure);
+hljs.registerLanguage('yaml', yaml);
+hljs.registerLanguage('django', django);
