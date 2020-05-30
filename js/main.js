e89467f40ccbb4d92b8ad4964a0a974a95dfdec1
Andros Fenollosa <andros@fenollosa.email>
2020-05-30 11:13:55 +0200

    Scroll aside

--- js/main.js
+++ js/main.js
@@ -5,9 +5,39 @@ import clojure from 'highlight.js/lib/languages/clojure';
 import yaml from 'highlight.js/lib/languages/yaml';
 import django from 'highlight.js/lib/languages/django';
 
-// Load languages highlight
-hljs.initHighlightingOnLoad();
-hljs.registerLanguage('shell', shell);
-hljs.registerLanguage('clojure', clojure);
-hljs.registerLanguage('yaml', yaml);
-hljs.registerLanguage('django', django);
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Load languages highlight
+    hljs.initHighlightingOnLoad();
+    hljs.registerLanguage('shell', shell);
+    hljs.registerLanguage('clojure', clojure);
+    hljs.registerLanguage('yaml', yaml);
+    hljs.registerLanguage('django', django);
+
+    // Scroll aside
+    //// Variables
+    const extraEspace = 50;
+    const speed = 1;
+    const delay = 2000;
+    const aside = document.querySelector('.aside');
+    const asideContent = document.querySelector('.aside__content');
+    let interval = undefined;
+
+    //// Event mouse
+    aside.addEventListener('mousemove', e => {
+        // Move
+        asideContent.style.transition = 'initial';
+        let heightWindow = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+        let difference =  (asideContent.offsetHeight + extraEspace) - heightWindow;
+        if (difference > 0) {
+            let porcetageMouse = Math.round(e.clientY * 100 / heightWindow);
+            asideContent.style.transform = `translateY(-${ difference * porcetageMouse / 100 }px)`;
+        }
+        // Return position start
+        clearInterval(interval);
+        interval = setInterval(() => {
+            asideContent.style.transition = `${speed}s`;
+            asideContent.style.transform = `translateY(0)`;
+        }, delay);
+    });
+});
